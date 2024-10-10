0fd777947aeeb00d15753c51cd0f15d66754d84d
aa <random_82@mail.ru>
2024-10-10 17:15:40 +0500

    Заголовок в файле java [#505272]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void rubleToEuro() {
+    }
+
+    @Test
+    void rubleToDollar() {
+    }
+}
