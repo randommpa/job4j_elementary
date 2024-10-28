75de26164296654895472bc24e8879271e2ea455
aa <random_82@mail.ru>
2024-10-28 14:07:10 +0500

    1.1. Что такое import [#505278 #518501]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,22 +1,22 @@
 package ru.job4j.calculator;
-import  ru.job4j.math.*;
+import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second);
+        return sum(first, second)
+                + multiply(first, second);
     }
 
     public static double subAndDiv(double first, double second) {
-        return MathFunction.subtract(first, second)
-                + MathFunction.division(first, second);
+        return subtract(first, second)
+                + division(first, second);
     }
 
     public static double sumAll(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second)
-                + MathFunction.division(first, second)
-                + MathFunction.subtract(first, second);
+        return sum(first, second)
+                + multiply(first, second)
+                + division(first, second)
+                + subtract(first, second);
     }
 
     public static void main(String[] args) {
